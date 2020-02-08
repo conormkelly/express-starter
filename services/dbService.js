03ad7283d8a0a1d3255228e450e4fb0a23a82430
Conor Kelly <conormpkelly@gmail.com>
2020-02-08 22:49:22 +0000

    feat: dbService to connect to mongoose

---
+++ services/dbService.js
@@ -0,0 +1,25 @@
+const logger = require('../utils/logger');
+
+const mongoose = require('mongoose');
+
+/**
+ * Connect to the database specified by _MONGO_URI_ in environment variabes.
+ */
+exports.connect = async () => {
+  const { MONGO_URI } = process.env;
+  logger.info('DB: Connecting');
+
+  let conn;
+  try {
+    conn = await mongoose.connect(MONGO_URI, {
+      useNewUrlParser: true,
+      useFindAndModify: true,
+      useUnifiedTopology: true
+    });
+  } catch (err) {
+    logger.error('DB: Failed to connect');
+    throw err;
+  }
+
+  logger.info(`DB: Connected to ${conn.connection.host}`);
+};
--- utils/startup.js
+++ utils/startup.js
@@ -1,6 +1,7 @@
 const logger = require('./logger');
 
 const config = require('../config');
+const dbService = require('../services/dbService');
 
 /**
  * Initialize all pre-requisites before starting the application.
@@ -8,8 +9,9 @@ const config = require('../config');
 async function initialize() {
   try {
     await config.validateEnvironmentVariables();
+    await dbService.connect();
   } catch (err) {
-    logger.error({ message: err.message, category: 'STARTUP' });
+    logger.error({ message: `${err.name}: ${err.message}`, category: 'STARTUP' });
     process.exit(1);
   }
 }
