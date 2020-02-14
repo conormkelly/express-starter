08fc5b1f0c265e8100eae37525f56ae500ab8fab
Conor Kelly <conormpkelly@gmail.com>
2020-02-14 23:52:52 +0000

    error-handling: improve ErrorResponse and guard against invalid JSON request body

--- app.js
+++ app.js
@@ -6,17 +6,24 @@ const addTraceId = require('express-request-id');
 const httpContext = require('express-http-context');
 const { expressWinstonLogger, expressWinstonErrorLogger } = require('./utils/logger');
 const router = require('./routes');
+const invalidJSONBodyHandler = require('./middleware/invalidJSONBodyHandler');
 const invalidRouteHandler = require('./middleware/invalidRouteHandler');
 const errorHandler = require('./middleware/errorHandler');
 
 const { start } = require('./utils/startup');
 
-// Middleware
+//* Middleware
 app.use(helmet());
-app.use(express.json());
 
 // Add traceId to all requests, for correlation in the logs
 app.use(addTraceId({ attributeName: 'traceId' }));
+
+// Log info on all requests
+app.use(expressWinstonLogger);
+
+// Marshall request body into JSON
+app.use(express.json());
+app.use(invalidJSONBodyHandler);
 
 // Allows us to get traceId in the logger module
 app.use(httpContext.middleware);
@@ -25,19 +32,17 @@ app.use((req, res, next) => {
   next();
 });
 
-// Log info on all requests
-app.use(expressWinstonLogger);
-
-// Mount routes
+//* Mount routes
 app.use('/api/v1', router);
 
+//* Error handling
 // Handle 404s
 app.use(invalidRouteHandler);
 
 // Log errors
 app.use(expressWinstonErrorLogger);
 
-// Form error responses
+// Respond with santized error messages
 app.use(errorHandler);
 
 start(app);
--- utils/error-handling/ErrorResponse.js
+++ utils/error-handling/ErrorResponse.js
@@ -2,17 +2,18 @@
  * Used to encapsulate business errors and allows us
  * to cleanly return client-friendly error messages.
  */
-
 class ErrorResponse extends Error {
-  constructor(message, statusCode, options) {
-    super();
-    this.message = message;
+  /**
+   * @param {{message: string, details: string, statusCode: number, shouldIncludeStack: boolean}} errorResponse
+   */
+  constructor({message, statusCode, details, shouldIncludeStack}) {
+    super(message);
     this.statusCode = statusCode;
-
+    this.details = details;
     // Controls whether to log the stackTrace in expressWinstonErrorLogger.
     // By default, exclude the stack from being logged for known /
     // "business errors" unless explicitly specified.
-    this.shouldIncludeStack = (options && options.shouldIncludeStack) || false;
+    this.shouldIncludeStack = shouldIncludeStack || false;
   }
 }
 
