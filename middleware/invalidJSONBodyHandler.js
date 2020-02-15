b181e6e13edb798a350334b1f22a104752121aff
Conor Kelly <conormpkelly@gmail.com>
2020-02-15 00:14:01 +0000

    chore: add jsdoc to invalidJSONBodyHandler, format

--- middleware/invalidJSONBodyHandler.js
+++ middleware/invalidJSONBodyHandler.js
@@ -1,10 +1,18 @@
 const ErrorResponse = require('../utils/error-handling/ErrorResponse');
 
+/**
+ * Returns a formulated error response if express.json / bodyparser throws an error.
+ */
 const invalidJSONBodyHandler = (err, req, res, next) => {
   if (err && err instanceof SyntaxError && 'body' in err) {
-    return next(new ErrorResponse({message: 'Invalid JSON body provided.', statusCode: 400}));
+    return next(
+      new ErrorResponse({
+        message: 'Invalid JSON body provided.',
+        statusCode: 400
+      })
+    );
   }
   next();
-}
+};
 
 module.exports = invalidJSONBodyHandler;
